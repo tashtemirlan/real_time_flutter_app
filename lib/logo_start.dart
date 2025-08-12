d30e8bf428cd70138bd5e9cce0579b6771e08886
Teit <teitteiterlan@gmail.com>
2025-08-12 16:17:33 +0600

    Created logo start and first UI

--- lib/logo_start.dart
+++ lib/logo_start.dart
@@ -1,8 +1,12 @@
 import 'dart:async';
-import 'dart:convert';
-import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:hive/hive.dart';
+import 'package:translator_real_time/translate_home.dart';
+
+import 'globals.dart';
 
 
 class LogoStart extends StatefulWidget{
@@ -21,352 +25,39 @@ class LogoStartState extends State<LogoStart>{
     bool isLanguageSet = box.containsKey("Language");
     if(isLanguageSet){
       String language = box.get("Language");
-      globals.userLanguage = language;
+      userLanguage = language;
     }
     else{
-      globals.userLanguage = "ru";
-    }
-  }
-
-  Future<bool> isUserLogged() async{
-    //check our logic =>
-    var box = await Hive.openBox("logins");
-    bool isRefreshTokenInHive= box.containsKey("refresh");
-    bool isAccessTokenInHive= box.containsKey("access");
-    if(isRefreshTokenInHive && isAccessTokenInHive){
-      String refresh = box.get("refresh");
-
-      final dio = Dio();
-      dio.options.headers['Accept-Language'] = globals.userLanguage;
-      dio.options.connectTimeout = const Duration(seconds: 30);
-      dio.options.receiveTimeout = const Duration(seconds: 30);
-      try{
-        //try to refresh our tokens =>
-
-        final respose = await dio.post(globals.endpointRefreshTokens, data: {"refresh" : refresh});
-        if(respose.statusCode == 200){
-          String toParseData = respose.toString();
-
-          Map<String, dynamic> jsonData = jsonDecode(toParseData);
-          // Access the values
-          String refresh = jsonData['refresh'];
-          String access = jsonData['access'];
-
-          box.put("refresh", refresh);
-          box.put("access", access);
-
-
-          globals.isUserRegistered = true;
-          return true;
-        }
-        else{
-          return false;
-        }
-      }
-      catch(error){
-        if(error is DioException){
-          if (error.response != null) {
-            String toParseData = error.response.toString();
-            dynamic data = jsonDecode(toParseData);
-            String loginErrorMessage = data['detail'];
-            Fluttertoast.showToast(
-              msg: loginErrorMessage,
-              toastLength: Toast.LENGTH_SHORT,
-              gravity: ToastGravity.BOTTOM, // Position of the toast on the screen
-              backgroundColor: Colors.white, // Background color of the toast
-              textColor: Colors.black,
-              fontSize: 12.0,
-            );
-          }
-        }
-      }
-      return false;
-    }
-    else{
-      globals.isUserRegistered = false;
-      return false;
-    }
-  }
-
-  Future<bool> getAppVersion() async{
-    final dio = Dio();
-    //set Dio response =>
-    dio.options.headers['Accept-Language'] = globals.userLanguage;
-    dio.options.connectTimeout = const Duration(seconds: 30);
-    dio.options.receiveTimeout = const Duration(seconds: 30);
-    try{
-      final response = await dio.get(globals.serverAppVersionGetEndpoint);
-      if(response.statusCode == 200){
-        final result = appVersionDataClassFromJson(response.toString());
-        appVersionString = result.result!.first.appVersion!;
-        digits = appVersionString.split('.').map(int.parse).toList();
-        verIntData = (digits[0]*100) + (digits[1]*10) + digits[2];
-        return true;
-      }
-    }
-    catch(error){
-      if(error is DioException){
-        if (error.response != null) {
-          String toParseData = error.response.toString();
-          print(toParseData);
-        }
-      }
-    }
-    return false;
-  }
-
-  Future<void> _launchURL(Uri url) async {
-    await launchUrl(url);
-  }
-
-  void showOutDateDialog(){
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        double width = MediaQuery.of(context).size.width;
-        return Dialog(
-          backgroundColor: Colors.white,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(28.0),
-          ),
-          child: SizedBox(
-            width: 150,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const SizedBox(height: 10,),
-                Text((globals.userLanguage == "ru")? versionOutdatedStringTitleRu : versionOutdatedStringTitleKg, textAlign: TextAlign.center,
-                    style: const TextStyle(
-                        fontSize: 20, color: Colors.black, fontWeight: FontWeight.w500 , letterSpacing: 0.2
-                    )
-                ),
-                const SizedBox(height: 10,),
-                Text((globals.userLanguage == "ru")? versionOutdatedStringSubTitleRu : versionOutdatedStringSubTitleKg, textAlign: TextAlign.center,
-                    style: const TextStyle(
-                        fontSize: 20, color: Colors.black, fontWeight: FontWeight.w500 , letterSpacing: 0.2
-                    )
-                ),
-                const SizedBox(height: 20,),
-                GestureDetector(
-                  onTap: (){
-                    //todo : => here we need navigate user to a newer version :
-                    if(Platform.isIOS){
-                      final String urlIOSPath = "https://apps.apple.com/kg/app/zherdesh/id6478204121";
-                      Uri uri = Uri.parse(urlIOSPath);
-                      _launchURL(uri);
-                    }
-                    if(Platform.isAndroid){
-                      final String urlAndroidPath = "https://play.google.com/store/apps/details?id=com.zherdeshapp.zherdeshmobileapplication&hl=ru";
-                      Uri uri = Uri.parse(urlAndroidPath);
-                      _launchURL(uri);
-                    }
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Container(
-                      width: width,
-                      decoration: BoxDecoration(
-                          color: mainZherdeshColor,
-                          borderRadius: BorderRadius.circular(20)
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 10),
-                        child: Text((globals.userLanguage == "ru")? buttonDataStringRu : buttonDataStringKg, textAlign: TextAlign.center,
-                            style: const TextStyle(
-                                fontSize: 20, color: Colors.white, fontWeight: FontWeight.w500 , letterSpacing: 0.2
-                            )
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20,),
-              ],
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-  void noUserHaveInternet(){
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        double width = MediaQuery.of(context).size.width;
-        return Dialog(
-          backgroundColor: Colors.white,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(28.0),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const SizedBox(height: 10,),
-              Text((globals.userLanguage == "ru")? noInternetRu : noInternetKg, textAlign: TextAlign.center,
-                  style: const TextStyle(
-                      fontSize: 20, color: Colors.black, fontWeight: FontWeight.w500 , letterSpacing: 0.2
-                  )
-              ),
-              const SizedBox(height: 20,),
-              GestureDetector(
-                onTap: () async{
-                  await retryFetchData();
-                },
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Container(
-                    width: width,
-                    decoration: BoxDecoration(
-                        color: mainZherdeshColor,
-                        borderRadius: BorderRadius.circular(20)
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 10),
-                      child: Text((globals.userLanguage == "ru")? buttonDataStringRu : buttonDataStringKg, textAlign: TextAlign.center,
-                          style: const TextStyle(
-                              fontSize: 20, color: Colors.white, fontWeight: FontWeight.w500 , letterSpacing: 0.2
-                          )
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20,),
-            ],
-          ),
-        );
-      },
-    );
-  }
-
-  Future<void> retryFetchData() async{
-    bool userHaveInternet = await getAppVersion();
-    if(userHaveInternet){
-      await AppMetrica.reportEvent('Открыто приложение');
-      if(globals.globalVersion >= verIntData){
-        bool userLogged = await isUserLogged();
-        if(userLogged){
-          if(globals.userLanguage=="ru"){
-            Fluttertoast.showToast(
-              msg: "Добро пожаловать!",
-              toastLength: Toast.LENGTH_LONG,
-              gravity: ToastGravity.BOTTOM,
-              backgroundColor: Colors.white,
-              textColor: Colors.black,
-              fontSize: 12.0,
-            );
-          }
-          else{
-            Fluttertoast.showToast(
-              msg: "Кош келиңиз!",
-              toastLength: Toast.LENGTH_LONG,
-              gravity: ToastGravity.BOTTOM,
-              backgroundColor: Colors.white,
-              textColor: Colors.black,
-              fontSize: 12.0,
-            );
-          }
-        }
-        final navController = ref.read(navIndexProvider.notifier);
-        navController.setIndex(0);
-        context.goNamed(routes.GoRoutePath.homePageRoute);
-      }
-      else{
-        //If version is outdated we need to show alert dialog with excuses
-        showOutDateDialog();
-      }
-    }
-    else{
-      // if user don't have internet
-      noUserHaveInternet();
+      userLanguage = "ru";
     }
   }
 
   Future<void> logoMainMethod() async{
     //get and set user data for system
     await checkLaunchData();
-    //check Internet connection :
-    bool userHaveInternet = await getAppVersion();
-    if(userHaveInternet){
-      await AppMetrica.reportEvent('Открыто приложение');
-      if(globals.globalVersion >= verIntData){
-        bool userLogged = await isUserLogged();
-        if(userLogged){
-          if(globals.userLanguage=="ru"){
-            Fluttertoast.showToast(
-              msg: "Добро пожаловать!",
-              toastLength: Toast.LENGTH_LONG,
-              gravity: ToastGravity.BOTTOM,
-              backgroundColor: Colors.white,
-              textColor: Colors.black,
-              fontSize: 12.0,
-            );
-          }
-          else{
-            Fluttertoast.showToast(
-              msg: "Кош келиңиз!",
-              toastLength: Toast.LENGTH_LONG,
-              gravity: ToastGravity.BOTTOM,
-              backgroundColor: Colors.white,
-              textColor: Colors.black,
-              fontSize: 12.0,
-            );
-          }
-        }
-        final navController = ref.read(navIndexProvider.notifier);
-        navController.setIndex(0);
-        context.goNamed(routes.GoRoutePath.homePageRoute);
-      }
-      else{
-        //If version is outdated we need to show alert dialog with excuses
-        showOutDateDialog();
-      }
+    if(userLanguage=="ru"){
+      Fluttertoast.showToast(
+        msg: "Добро пожаловать!",
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.white,
+        textColor: Colors.black,
+        fontSize: 12.0,
+      );
     }
     else{
-      // if user don't have internet
-      noUserHaveInternet();
+      Fluttertoast.showToast(
+        msg: "Кош келиңиз!",
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.white,
+        textColor: Colors.black,
+        fontSize: 12.0,
+      );
     }
+    Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (BuildContext context) => const TranslateHome()));
   }
-
-  Widget casualTheme(double width , double height){
-    return
-  }
-
-  Widget newYearTheme(double width, double height){
-    return Scaffold(
-        resizeToAvoidBottomInset: false,
-        backgroundColor: newYearBackgroundColor,
-        body: Align(
-          alignment: Alignment.center,
-          child: Container(
-            width: width,
-            height: height,
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("assets/images/snow_falling.gif"),
-                    fit: BoxFit.cover)
-            ),
-            child: Container(
-              width: width*0.75,
-              height: height,
-              decoration: const BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage("assets/images/logo_new_year_zherdesh.png"),
-                      fit: BoxFit.fitWidth)
-              ),
-            ),
-          ),
-        )
-    );
-  }
-
 
   @override
   void dispose() {
@@ -376,6 +67,7 @@ class LogoStartState extends State<LogoStart>{
   @override
   void initState() {
     super.initState();
+    logoMainMethod();
   }
 
 
@@ -386,17 +78,19 @@ class LogoStartState extends State<LogoStart>{
 
     return Scaffold(
         resizeToAvoidBottomInset: false,
-        backgroundColor: scaffoldColor,
+        backgroundColor: const Color.fromRGBO(250, 250, 250, 1),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Padding(padding: EdgeInsets.only(left: width*0.3), child :
-            Container(
-              width: width*0.4,
-              height: height*0.4,
-              decoration: const BoxDecoration(image: DecorationImage(image: AssetImage("assets/images/logo.png"),fit: BoxFit.contain)),
-            )),
+            Padding(
+                padding: EdgeInsets.only(left: width*0.3),
+                child : FaIcon(
+                  FontAwesomeIcons.language ,
+                  size: width*0.4,
+                  color: Colors.blue.shade300,
+                )
+            ),
           ],
         )
     );
---
+++ lib/translate_home.dart
@@ -0,0 +1,147 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+
+
+
+class TranslateHome extends StatefulWidget {
+  const TranslateHome({super.key});
+
+  @override
+  TranslateHomeState createState() => TranslateHomeState();
+}
+
+class TranslateHomeState extends State<TranslateHome> {
+  String sourceLang = "English";
+  String targetLang = "Spanish";
+  final TextEditingController _inputController = TextEditingController();
+  String _translatedText = "";
+
+  void _swapLanguages() {
+    setState(() {
+      final temp = sourceLang;
+      sourceLang = targetLang;
+      targetLang = temp;
+    });
+  }
+
+  Future<void> _translateText() async {
+    // Here we will integrate google_mlkit_translation later
+    setState(() {
+      _translatedText = "Translated version of: ${_inputController.text}";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Translate'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          // Language selector row
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+            color: Theme.of(context).colorScheme.surfaceVariant,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                DropdownButton<String>(
+                  value: sourceLang,
+                  underline: const SizedBox(),
+                  items: const [
+                    DropdownMenuItem(value: "English", child: Text("English")),
+                    DropdownMenuItem(value: "Spanish", child: Text("Spanish")),
+                    DropdownMenuItem(value: "French", child: Text("French")),
+                  ],
+                  onChanged: (val) {
+                    setState(() => sourceLang = val!);
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(Icons.swap_horiz),
+                  onPressed: _swapLanguages,
+                ),
+                DropdownButton<String>(
+                  value: targetLang,
+                  underline: const SizedBox(),
+                  items: const [
+                    DropdownMenuItem(value: "English", child: Text("English")),
+                    DropdownMenuItem(value: "Spanish", child: Text("Spanish")),
+                    DropdownMenuItem(value: "French", child: Text("French")),
+                  ],
+                  onChanged: (val) {
+                    setState(() => targetLang = val!);
+                  },
+                ),
+              ],
+            ),
+          ),
+
+          // Input field
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: TextField(
+                controller: _inputController,
+                maxLines: null,
+                decoration: InputDecoration(
+                  hintText: 'Enter text',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  suffixIcon: IconButton(
+                    icon: const Icon(Icons.mic),
+                    onPressed: () {
+                      // TODO: Implement speech-to-text
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          // Output field
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(12),
+              margin: const EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey.shade400),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: SingleChildScrollView(
+                child: Text(
+                  _translatedText,
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+              ),
+            ),
+          ),
+
+          // Translate button
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(double.infinity, 50),
+              ),
+              onPressed: _translateText,
+              icon: const Icon(Icons.translate),
+              label: const Text("Translate"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
